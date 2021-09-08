b76bf13287f66a6a3c2ba6670e3fa1cad50bf241
Luiz Claudio <53128447+luizclaudio83@users.noreply.github.com>
2021-09-07 23:46:20 -0300

    Update generate-sw.js

--- generate-sw.js
+++ generate-sw.js
@@ -1,74 +1,76 @@
-const VERSION = "1.0.0";
+/*
+Copyright 2015, 2019 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 
-const APP_CACHE_NAME = 'harsensor-app';
-const STATIC_CACHE_NAME = 'hs-static';
+// Incrementing OFFLINE_VERSION will kick off the install event and force
+// previously cached resources to be updated from the network.
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
+// Customize this with a different URL if needed.
+const OFFLINE_URL = 'offline.html';
 
-//rotas dos arquivos estáticos
-const CACHE_STATIC = [
-    '/css/jquery.mobile-1.4.5.css',
-    '/image/icon-256x256.png',
-    '/image/icon-192x192.png',
-    '/image/icon-384x384.png',
-    '/image/icon-512x512.png',
-    '/img/logo.png',
- ];
-
-//rotas das paginas do site
- const CACHE_APP = [
-    '/',
-    '/www',
-    
-  
- ];
-
-self.addEventListener('install',function(e){
-    e.waitUntil(
-        Promise.all([
-            caches.open(STATIC_CACHE_NAME),
-            caches.open(APP_CACHE_NAME),
-            self.skipWaiting()
-          ]).then(function(storage){
-            var static_cache = storage[0];
-            var app_cache = storage[1];
-            return Promise.all([
-              static_cache.addAll(CACHE_STATIC),
-              app_cache.addAll(CACHE_APP)]);
-        })
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    // Setting {cache: 'reload'} in the new request will ensure that the response
+    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+  })());
 });
 
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
-        Promise.all([
-            self.clients.claim(),
-            caches.keys().then(function(cacheNames) {
-                return Promise.all(
-                    cacheNames.map(function(cacheName) {
-                        if (cacheName !== APP_CACHE_NAME && cacheName !== STATIC_CACHE_NAME) {
-                            console.log('deleting',cacheName);
-                            return caches.delete(cacheName);
-                        }
-                    })
-                );
-            })
-        ])
-    );
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
 });
 
-this.addEventListener('fetch', function(event) {
-  var response;
-  event.respondWith(caches.match(event.request)
-    .then(function (match) {
-      return match || fetch(event.request);
-    }).catch(function() {
-      return fetch(event.request);
-    })
-    .then(function(r) {
-      response = r;
-      caches.open(APP_CACHE_NAME).then(function(cache) {
-        cache.put(event.request, response);
-      });
-      return response.clone();
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        // First, try to use the navigation preload response if it's supported.
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        // catch is only triggered if an exception is thrown, which is likely
+        // due to a network error.
+        // If fetch() returns a valid HTTP response with a response code in
+        // the 4xx or 5xx range, the catch() will NOT be called.
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
+  }
+
+  // If our if() condition is false, then this fetch handler won't intercept the
+  // request. If there are any other fetch handlers registered, they will get a
+  // chance to call event.respondWith(). If no fetch handlers call
+  // event.respondWith(), the request will be handled by the browser as if there
+  // were no service worker involvement.
 });
